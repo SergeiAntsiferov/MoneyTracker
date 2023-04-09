758c7eaf74dbf4941db81d13e08c2d5820176433
Сергей Анциферов <s.antsiferov@unicon.ru>
2023-04-09 18:39:13 +0300

    Separated deployment

--- app.mjs
+++ app.mjs
@@ -1,13 +1,13 @@
 import express, { json } from "express"; // express library
 import cors from "cors"; // CORS module to prevent CORS errors 
 import path from "path"
-// import ('./routes/routes.mjs')
+import ('./routes/routes.mjs')
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const app = express(); // use express library
 export const jsonParser = json() // JSON parsing
-const __dirname = path.resolve(path.dirname(''))
+// const __dirname = path.resolve(path.dirname(''))
 
 app.use(cors()) // use CORS 
 
@@ -17,22 +17,26 @@ if (port) {
     console.log("Server is running on " + port + " port")
 }
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
-    app.get("*", (req, res) => {
-        res.sendFile(
-            path.resolve(__dirname, 'frontend', 'build', 'index.html'),
-            (error) => {
-                if(error) res.status(500).send(error)
-            }
-        );
-    })
-    app.post("*", (req, res) => {
-        res.sendFile(
-            path.resolve(__dirname, 'frontend', 'build', 'index.html'),
-            (error) => {
-                if(error) res.status(500).send(error)
-            }
-        );
-    })
-}
+app.get("/", (req, res) => {
+    res.json("test transactions server")
+})
+
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
+//     app.get("*", (req, res) => {
+//         res.sendFile(
+//             path.resolve(__dirname, 'frontend', 'build', 'index.html'),
+//             (error) => {
+//                 if(error) res.status(500).send(error)
+//             }
+//         );
+//     })
+//     app.post("*", (req, res) => {
+//         res.sendFile(
+//             path.resolve(__dirname, 'frontend', 'build', 'index.html'),
+//             (error) => {
+//                 if(error) res.status(500).send(error)
+//             }
+//         );
+//     })
+// }
