2c77e31bebd0153dcd60638d8ea5e80b5b904b64
Сергей Анциферов <s.antsiferov@unicon.ru>
2023-04-09 17:32:36 +0300

    delete build

--- frontend/src/utils/functions/basic.js
+++ frontend/src/utils/functions/basic.js
@@ -10,6 +10,7 @@ const controllers = {}; // controllers
 
 // sending/requesting function
 export async function sendData(method, url, data) {
+  console.log(method, url, data);
   try {
     // check existing controller for current url
     if (controllers[url]) { // if controller exist
@@ -30,6 +31,7 @@ export async function sendData(method, url, data) {
       });
       const result = await response.json();
       delete controllers[url]; // delete controller after request is done
+      console.log(result);
       return result;
     }
   } catch (error) {
