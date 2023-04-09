0329959a7c344c23b34b228608069a24d4054c78
Сергей Анциферов <s.antsiferov@unicon.ru>
2023-04-09 18:53:06 +0300

    changing extension app file

--- app.mjs
+++ app.mjs
@@ -1,42 +1,17 @@
 import express, { json } from "express"; // express library
 import cors from "cors"; // CORS module to prevent CORS errors 
-import path from "path"
 import ('./routes/routes.mjs')
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const app = express(); // use express library
 export const jsonParser = json() // JSON parsing
-// const __dirname = path.resolve(path.dirname(''))
 
 app.use(cors()) // use CORS 
 
 let port = process.env.API_PORT
-if (port) {
-    app.listen(port) // port listening
-    console.log("Server is running on " + port + " port")
-}
+if (port) app.listen(port) // port listening
 
 app.get("/", (req, res) => {
-    res.json("test transactions server")
+    res.send("test transactions server on " + port + " port")
 })
-
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
-//     app.get("*", (req, res) => {
-//         res.sendFile(
-//             path.resolve(__dirname, 'frontend', 'build', 'index.html'),
-//             (error) => {
-//                 if(error) res.status(500).send(error)
-//             }
-//         );
-//     })
-//     app.post("*", (req, res) => {
-//         res.sendFile(
-//             path.resolve(__dirname, 'frontend', 'build', 'index.html'),
-//             (error) => {
-//                 if(error) res.status(500).send(error)
-//             }
-//         );
-//     })
-// }
