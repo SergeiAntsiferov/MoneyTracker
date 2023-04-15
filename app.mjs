15614b9502fef25870284e6236c6187e18b230e3
Сергей Анциферов <s.antsiferov@unicon.ru>
2023-04-15 21:19:05 +0300

    upd code

--- app.mjs
+++ app.mjs
@@ -1,10 +1,9 @@
 import express, { json } from "express"; // express library
 import cors from "cors"; // CORS module to prevent CORS errors 
 import catch_handler from './functions/catch_handler.mjs'
-import { db, sample_supplies, uri } from './connections/mongo_connect.mjs'
+import { db, sample_supplies } from './connections/mongo_connect.mjs'
 import { ObjectID } from './connections/mongo_connect.mjs';
 import * as dotenv from 'dotenv';
-
 dotenv.config();
 
 const app = express(); // use express library
@@ -56,31 +55,30 @@ app.post ("/api/change_theme", jsonParser, async (request, response) => {
 
 // Get transactions
 app.post("/api/get_transactions", jsonParser, async (request, response) => {
-    response.json({ test: uri})
-    // try {
-    //     const { range, sort } = request.body
-    //     const sales = sample_supplies.collection('sales');
-    //     let result;
+    try {
+        const { range, sort } = request.body
+        const sales = sample_supplies.collection('sales');
+        let result;
 
-    //     if (range) {
-    //         const prapareRange = range.map(id => ObjectID(id))
-    //         result = await sales.find({ _id : { $in : prapareRange } }).toArray() // get data in range
+        if (range) {
+            const prapareRange = range.map(id => ObjectID(id))
+            result = await sales.find({ _id : { $in : prapareRange } }).toArray() // get data in range
                         
-    //     } else if (sort) {
-    //         const { field, sorting } = sort
-    //         if (!sorting) result = await sales.distinct("_id", {}) // get only id
-    //         else {
-    //             let array = [] // array for results
-    //             await sales.find({}, {
-    //                 sort: { [field]: sorting }, // sorting parameter
-    //                 projection: { "_id": 1 } // necessary fields
-    //             }).forEach(item => array.push(item["_id"])) // write results to array
-    //             result = array
-    //         }
-    //     } else result = await sales.distinct("_id", {}) // get only id
+        } else if (sort) {
+            const { field, sorting } = sort
+            if (!sorting) result = await sales.distinct("_id", {}) // get only id
+            else {
+                let array = [] // array for results
+                await sales.find({}, {
+                    sort: { [field]: sorting }, // sorting parameter
+                    projection: { "_id": 1 } // necessary fields
+                }).forEach(item => array.push(item["_id"])) // write results to array
+                result = array
+            }
+        } else result = await sales.distinct("_id", {}) // get only id
 
-    //     response.json(result) // send response to frontend
-    // } catch(error) { 
-    //     catch_handler(error, "/get_transactions") 
-    // }
+        response.json(result) // send response to frontend
+    } catch(error) { 
+        catch_handler(error, "/get_transactions") 
+    }
 });
